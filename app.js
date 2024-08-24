b6783524f51a6d876c808d3e40483f783c2f44c8
Spanthyx <69044190+Spanthyx@users.noreply.github.com>
2024-08-24 23:44:53 +0200

    Add background-color change when hovering over square

--- app.js
+++ app.js
@@ -6,8 +6,11 @@ for (let i = 0; i < 16; i++) {
     row.setAttribute("class", "row");
     for (let j = 0; j < 16; j++) {
         let square = document.createElement("div");
-        square.setAttribute("style", "flex: 1; border: 1px solid black;");
+        square.setAttribute("style", "flex: 1;");
         square.setAttribute("class", "square");
+        square.addEventListener("mouseenter", () => {
+            square.style.backgroundColor = "pink";
+          });
         row.appendChild(square);
     }
     container.appendChild(row);
